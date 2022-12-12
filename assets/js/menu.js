778127956916b724290da2f34e0ceb875305082c
miyazaki-hitoki <120359270+miyazaki-hitoki@users.noreply.github.com>
2022-12-12 15:01:18 +0900

    Add files via upload

---
+++ assets/js/menu.js
@@ -0,0 +1,39 @@
+$('.l-header__container__menu__btn').on('click', function(){
+  $('.l-header__container__menu__container').toggleClass('active');
+});
+$(".l-header__container__menu__container__list__items").click(function () {
+  $(".l-header__container__menu__container").toggleClass('active');
+});
+$('.p-main').on('click', function(){
+  $('.l-header__container__menu__container').removeClass('active');
+});
+$('.p-works').on('click', function(){
+  $('.l-header__container__menu__container').removeClass('active');
+});
+$('.p-wlist').on('click', function(){
+  $('.l-header__container__menu__container').removeClass('active');
+});
+$('.p-works_btob').on('click', function(){
+  $('.l-header__container__menu__container').removeClass('active');
+});
+$('.p-works_btoc').on('click', function(){
+  $('.l-header__container__menu__container').removeClass('active');
+});
+$('.p-works_mutoh').on('click', function(){
+  $('.l-header__container__menu__container').removeClass('active');
+});
+$('.p-works_car').on('click', function(){
+  $('.l-header__container__menu__container').removeClass('active');
+});
+$('.p-works_gaiso').on('click', function(){
+  $('.l-header__container__menu__container').removeClass('active');
+});
+$('.p-works_hba').on('click', function(){
+  $('.l-header__container__menu__container').removeClass('active');
+});
+$('.p-works_iinexus').on('click', function(){
+  $('.l-header__container__menu__container').removeClass('active');
+});
+$('.p-works__ryoko').on('click', function(){
+  $('.l-header__container__menu__container').removeClass('active');
+});
